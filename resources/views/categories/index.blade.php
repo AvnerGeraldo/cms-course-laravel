713c63bc6cfb9a2d08e8486a6c167c45f97c831d
Avner <avner.conceicao@hotmail.com>
2020-04-19 19:07:31 -0300

    refactor(category): changing the 'Add Category' button into the card header

--- resources/views/categories/index.blade.php
+++ resources/views/categories/index.blade.php
@@ -2,11 +2,26 @@
 
 @section('content')
 
-    <div class="d-flex justify-content-end mb-2">
-        <a href="{{ route('categories.create') }}" class="btn btn-success">Add Category</a>
-    </div>
     <div class="card card-default">
-        <div class="card-header">Categories</div>
-        <div class="card-body"></div>
+        <div class="card-header">
+            <div class="float-left my-2">Categories</div>
+            <div class="float-right">
+                <a href="{{ route('categories.create') }}" class="btn btn-success">Add Category</a>
+            </div>
+        </div>
+        <div class="card-body">
+            <table class="table">
+                <thead>
+                    <th>Name</th>
+                </thead>
+                <tbody>
+                    @foreach ($listCategories as $category)
+                        <tr>
+                            <td>{{  $category->name }}</td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
     </div>
 @endsection
