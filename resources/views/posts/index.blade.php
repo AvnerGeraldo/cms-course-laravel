4fd21507abdb50072ea5d8935770512c7be333f4
Avner <avner.conceicao@hotmail.com>
2020-04-23 11:01:00 -0300

    feat(show posts): fetch posts from DB and display them on table

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -10,7 +10,25 @@
             </div>
         </div>
         <div class="card-body">
-
+            <table class="table">
+                <thead>
+                    <th>Image</th>
+                    <th>Title</th>
+                    <th></th>
+                </thead>
+                <tbody>
+                    @foreach ($posts as $post)
+                        <tr>
+                            <td><img src="{{ asset('storage/'.$post->image) }}" width="60" height="60" alt="{{ $post->title }}" /></td>
+                            <td>{{ $post->title }}</td>
+                            <td class="text-right">
+                                <a href="" class="btn btn-info btn-sm">Edit</a>
+                                <a href="" class="btn btn-danger btn-sm">Trash</a>
+                            </td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
         </div>
     </div>
 
