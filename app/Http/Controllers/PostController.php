e8ce662580fa6216d4acb68bc91c9c4423a69b27
Avner <avner.conceicao@hotmail.com>
2020-04-23 17:55:53 -0300

    feat(create post): add published_at field to store post

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -45,7 +45,8 @@ class PostController extends Controller
             'title' => $request->title,
             'description' => $request->description,
             'content' => $request->content,
-            'image' => $image
+            'image' => $image,
+            'published_at' => $request->published_at
         ]);
 
         //Flash a message
