b9108680e36db67086f6d9bbe2e76b94448f188d
Avner <avner.conceicao@hotmail.com>
2020-04-23 17:53:37 -0300

    feat(create post): add trix-editor and flatpickr to the post form

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -15,23 +15,24 @@
 
                 <div class="form-group">
                     <label for="title">Title</label>
-                    <input type="text" name="title" id="title" class="form-control">
+                    <input type="text" name="title" id="title" value="{{ old('title') }}" class="form-control">
                 </div>
                 <div class="form-group">
                     <label for="description">Description</label>
-                    <textarea cols="5" rows="5" name="description" id="description" class="form-control"></textarea>
+                    <textarea cols="5" rows="5" name="description" id="description" class="form-control">{{ old('description') }}</textarea>
                 </div>
                 <div class="form-group">
                     <label for="content">Content</label>
-                    <textarea cols="5" rows="5" name="content" id="content" class="form-control"></textarea>
+                    <input type="hidden" name="content" id="content" value="{{ old('content') }}"/>
+                    <trix-editor input="content"></trix-editor>
                 </div>
                 <div class="form-group">
                     <label for="image">Image</label>
-                    <input type="file" name="image" id="image" class="form-control">
+                    <input type="file" name="image" id="image" class="form-control" />
                 </div>
                 <div class="form-group">
                     <label for="published_at">Published At</label>
-                    <input type="text" name="published_at" id="published_at" class="form-control">
+                    <input type="text" name="published_at" id="published_at" value="{{ old('published_at') }}" class="form-control">
                 </div>
                 <div class="form-group">
                     <button type="submit"  class="btn btn-success">Create Post</button>
@@ -40,4 +41,19 @@
         </div>
     </div>
 
+@endsection
+
+@section('load-javascript')
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/trix/1.2.1/trix.js"></script>
+    <script type="text/javascript">
+        flatpickr('#published_at', {
+            dateFormat: 'd/m/Y H:i:ss',
+            enableTime: true,
+            locale: "pt"
+        });
+    </script>
+@endsection
+
+@section('load-css')
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/trix/1.2.1/trix.css" />
 @endsection
