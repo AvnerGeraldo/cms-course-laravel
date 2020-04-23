cab01af87b6fce20d8962808e02b19628dc069cf
Avner <avner.conceicao@hotmail.com>
2020-04-23 12:15:13 -0300

    feat(trashed posts): show trashed posts at the posts page

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -4,36 +4,47 @@
 
     <div class="card card-default">
         <div class="card-header">
-            <div class="float-left my-2">Posts</div>
-            <div class="float-right">
-                <a href="{{ route('posts.create') }}" class="btn btn-success">Add Posts</a>
-            </div>
+            <div class="float-left my-2">{{ isset($trashedPage) && $trashedPage ?  'Trashed Posts' : 'Posts' }}</div>
+            @if (!isset($trashedPage))
+                <div class="float-right">
+                    <a href="{{ route('posts.create') }}" class="btn btn-success">Add Posts</a>
+                </div>
+            @endif
         </div>
         <div class="card-body">
-            <table class="table">
-                <thead>
-                    <th>Image</th>
-                    <th>Title</th>
-                    <th></th>
-                </thead>
-                <tbody>
-                    @foreach ($posts as $post)
-                        <tr>
-                            <td><img src="{{ asset('storage/'.$post->image) }}" width="60" height="60" alt="{{ $post->title }}" /></td>
-                            <td>{{ $post->title }}</td>
-                            <td class="text-right">
-                                <form action="{{ route('posts.destroy', $post->id) }}" method="POST" class="float-right ml-2">
-                                    @csrf
-                                    @method('DELETE')
+            @if ($posts->count() > 0)
+                <table class="table">
+                    <thead>
+                        <th>Image</th>
+                        <th>Title</th>
+                        <th></th>
+                    </thead>
+                    <tbody>
+                        @foreach ($posts as $post)
+                            <tr>
+                                <td><img src="{{ asset('storage/'.$post->image) }}" width="60" height="60" alt="{{ $post->title }}" /></td>
+                                <td>{{ $post->title }}</td>
+                                <td class="text-right">
+                                    <form action="{{ route('posts.destroy', $post->id) }}" method="POST" class="float-right ml-2">
+                                        @csrf
+                                        @method('DELETE')
 
-                                    <button type="submit" onclick="return confirm('Do you wanna delete the post \'{{ $post->title}}\' ?');" class="btn btn-danger btn-sm">Trash</button>
-                                </form>
-                                <a href="" class="btn btn-info btn-sm float-right">Edit</a>
-                            </td>
-                        </tr>
-                    @endforeach
-                </tbody>
-            </table>
+                                        <button type="submit" onclick="return confirm('Do you wanna {{ $post->trashed() ?  'delete' : 'trash' }} the post \'{{ $post->title}}\' ?');" class="btn btn-danger btn-sm">
+                                            {{ $post->trashed() ?  'Delete' : 'Trash' }}
+                                        </button>
+                                    </form>
+                                    @if (!$post->trashed())
+                                        <a href="" class="btn btn-info btn-sm float-right">Edit</a>
+                                    @endif
+                                </td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            @else
+                <h3 class="text-center">No posts at this moment.</h3>
+            @endif
+
         </div>
     </div>
 
