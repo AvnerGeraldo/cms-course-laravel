f9af521eedf6c20090c2a8a8cd51ab69db5e9110
Avner <avner.conceicao@hotmail.com>
2020-04-23 17:54:43 -0300

    feat(post model): set a muttator for 'published_at' field to format date

--- app/Model/Post.php
+++ app/Model/Post.php
@@ -2,6 +2,7 @@
 
 namespace App\Model;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
@@ -17,4 +18,9 @@ class Post extends Model
     protected $image;
     protected $published_at;
     protected $fillable = ['title', 'description', 'content', 'image', 'published_at'];
+
+    public function setPublishedAtAttribute($value)
+    {
+        $this->attributes['published_at'] = Carbon::createFromFormat('d/m/Y H:i:s', $value)->format('Y-m-d H:i:s');
+    }
 }
