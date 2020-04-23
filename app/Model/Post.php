21fef27fbf7d279c54bae459d4e669e26e3e16fa
Avner <avner.conceicao@hotmail.com>
2020-04-23 18:25:40 -0300

    feat(post model): format date of the published_at field to show on view

--- app/Model/Post.php
+++ app/Model/Post.php
@@ -19,6 +19,11 @@ class Post extends Model
     protected $published_at;
     protected $fillable = ['title', 'description', 'content', 'image', 'published_at'];
 
+    public function getPublishedAtAttribute($value)
+    {
+        return Carbon::createFromFormat('Y-m-d H:i:s', $value)->format('d/m/Y H:i:s');
+    }
+
     public function setPublishedAtAttribute($value)
     {
         $this->attributes['published_at'] = Carbon::createFromFormat('d/m/Y H:i:s', $value)->format('Y-m-d H:i:s');
